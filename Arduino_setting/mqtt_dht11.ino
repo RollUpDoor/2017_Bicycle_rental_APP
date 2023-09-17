8a0ddbcf50d968b9651f0408ddce6d31925f3b55
Hedegehog Eden (皓暄) <131877799+RollUpDoor@users.noreply.github.com>
2023-09-17 14:37:11 +0800

    Add files via upload

---
+++ Arduino_setting/mqtt_dht11.ino
@@ -0,0 +1,151 @@
+/*
+ Basic MQTT example
+
+ This sketch demonstrates the basic capabilities of the library.
+ It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic"
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+
+ It will reconnect to the server if the connection is lost using a blocking
+ reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+ achieve the same result without blocking the main loop.
+ 
+*/
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+
+//=============以下為DHT11========================
+#define DHTPIN 2     // what digital pin we're connected to
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+//=============以上為DHT11========================
+
+// Update these with values suitable for your network.
+char ssid[] = "CHT7050";     // your network SSID (name)
+char pass[] = "055372408";  // your network password
+int status  = WL_IDLE_STATUS;    // the Wifi radio's status
+
+//=============温濕度並給定初始值========================
+int temp = 30;
+int humd  = 50;
+
+char mqttServer[]     = "iot.eclipse.org";
+char clientId[]       = "Kuoching";
+char publishTopic[]   = "Kuoching01";
+char subscribeTopic[] = "Kuoching01";
+char publishPayload[] = "Temperature:60,Humidity:30";
+String publishPayloadStr = "Temperature:60,Humidity:30"; 
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(clientId)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(publishTopic, publishPayload);
+      // ... and resubscribe
+      client.subscribe(subscribeTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(38400);
+//=============以下為DHT11========================
+  Serial.println("DHTxx test!");
+  dht.begin();
+ //=============以上為DHT11========================
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
+
+  // Allow the hardware to sort itself out
+  delay(1500);
+}
+
+void loop()
+{
+//=============以下為DHT11========================
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  String publishPayloadStr = "Temperature:"+ String((int)t) + ",Humidity:" + String((int)h);
+  Serial.print("Message published：");
+  Serial.println(publishPayloadStr);
+  int size = publishPayloadStr.length()+1;
+  publishPayloadStr.toCharArray(publishPayload, size);
+ //=============以上為DHT11========================
+ if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+       // Once connected, publish an announcement...
+      client.publish(publishTopic, publishPayload);
+      delay(10000);
+}
